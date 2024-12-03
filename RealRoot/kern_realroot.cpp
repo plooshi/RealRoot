a2c65328f8d065ace57420eb214b57503410a4ff
Ploosh <me@ploosh.dev>
2024-12-02 22:11:12 -0500

    Finish Big Sur & Monterey support

--- RealRoot/kern_realroot.cpp
+++ RealRoot/kern_realroot.cpp
@@ -6,6 +6,39 @@
 
 #include "kern_realroot.hpp"
 
+struct segment_command_64 *get_segment(mach_header_64 *header, const char *name) {
+	auto lcmd = (struct load_command *) (header + 1);
+
+	for (int i = 0; i < header->ncmds; i++) {
+		if (lcmd->cmd == LC_SEGMENT_64) {
+			struct segment_command_64 *segment = (struct segment_command_64 *) lcmd;
+			if (strcmp(segment->segname, name) == 0) {
+				return segment;
+			}
+		} else {
+			break;
+		}
+
+		lcmd = (struct load_command *) ((char *) lcmd + lcmd->cmdsize);
+	}
+
+	return NULL;
+}
+
+struct section_64 *get_section(struct segment_command_64 *segment, const char *name) {
+	auto section = (struct section_64 *) (segment + 1);
+
+	for (int i = 0; i < segment->nsects; i++) {
+		if (strcmp(section->sectname, name) == 0) {
+			return section;
+		}
+
+		section++;
+	}
+
+	return NULL;
+}
+
 static const char *kextAPFSPath[] { "/System/Library/Extensions/apfs.kext/Contents/MacOS/apfs" };
 
 static KernelPatcher::KextInfo kextAPFS { "com.apple.filesystems.apfs", kextAPFSPath, 1, {true, true}, {}, KernelPatcher::KextInfo::Unloaded };
@@ -23,10 +56,16 @@ uint32_t RetOne() {
 }
 
 void PatchAPFS(KernelPatcher &patcher, size_t index, mach_vm_address_t address, size_t size) {
-	KernelPatcher::RouteRequest req("_apfs_root_snapshot_select", selectSnapshotPatch);
-	if (!patcher.routeMultiple(index, &req, 1))
-		panic("Failed to route apfs_root_snapshot_select!");
 	auto apfs_vfsop_mount = patcher.solveSymbol(index, "_apfs_vfsop_mount");
+	if (getKernelVersion() >= KernelVersion::Ventura) {
+		KernelPatcher::RouteRequest req("_apfs_root_snapshot_select", selectSnapshotPatch);
+		if (!patcher.routeMultiple(index, &req, 1))
+			panic("Failed to route apfs_root_snapshot_select!");
+	} else {
+		if (!KernelPatcher::findAndReplaceWithMask((void *) apfs_vfsop_mount, 32768, SnapshotOrig, arrsize(SnapshotOrig), SnapshotMask, arrsize(SnapshotMask), SnapshotReplace, arrsize(SnapshotReplace), SnapshotReplaceMask, arrsize(SnapshotReplaceMask))) {
+			panic("Failed to patch apfs_root_snapshot_select!");
+		}
+	}
 	if (getKernelVersion() >= KernelVersion::Sonoma) {
 		KernelPatcher::RouteRequest req("_apfs_mount_upgrade_checks", RetZero);
 		if (!patcher.routeMultiple(index, &req, 1))
@@ -36,16 +75,22 @@ void PatchAPFS(KernelPatcher &patcher, size_t index, mach_vm_address_t address, 
 		if (!patcher.routeMultiple(index, &req, 1))
 			panic("Failed to route apfs_allow_root_update!");
 	} else {
-		if (!KernelPatcher::findAndReplaceWithMask((void *) apfs_vfsop_mount, 32768, oldRWPatchOrig, sizeof(oldRWPatchOrig), oldRWPatchMask, sizeof(oldRWPatchMask), oldRWPatchReplace, sizeof(oldRWPatchReplace), oldRWPatchReplaceMask, sizeof(oldRWPatchReplaceMask))) {
+		if (!KernelPatcher::findAndReplaceWithMask((void *) apfs_vfsop_mount, 32768, oldRWPatchOrig, arrsize(oldRWPatchOrig), oldRWPatchMask, arrsize(oldRWPatchMask), oldRWPatchReplace, arrsize(oldRWPatchReplace), oldRWPatchReplaceMask, arrsize(oldRWPatchReplaceMask))) {
 			panic("Failed to patch apfs_vfsop_mount RW patch!");
 		}
 	}
 	size_t dataOffset = 0;
-	if (!KernelPatcher::findPattern(apfsVfsopMountOrig, apfsVfsopMountMask, sizeof(apfsVfsopMountOrig), (void *) apfs_vfsop_mount, 32768, &dataOffset)) {
-		panic("Failed to find apfs_vfsop_mount LiveFS patch!");
+	if (getKernelVersion() >= KernelVersion::Ventura) {
+		if (!KernelPatcher::findPattern(apfsVfsopMountOrig, apfsVfsopMountMask, arrsize(apfsVfsopMountOrig), (void *) apfs_vfsop_mount, 32768, &dataOffset)) {
+			panic("Failed to find apfs_vfsop_mount LiveFS patch!");
+		}
+	} else {
+		if (!KernelPatcher::findPattern(apfsVfsopMountOrigBSM, apfsVfsopMountMaskBSM, arrsize(apfsVfsopMountOrigBSM), (void *) apfs_vfsop_mount, 32768, &dataOffset)) {
+			panic("Failed to find apfs_vfsop_mount LiveFS patch!");
+		}
 	}
 	MachInfo::setKernelWriting(true, KernelPatcher::kernelWriteLock);
-	mach_vm_address_t patchPoint = apfs_vfsop_mount + dataOffset + 0x15;
+	mach_vm_address_t patchPoint = apfs_vfsop_mount + dataOffset + (getKernelVersion() >= KernelVersion::Ventura ? 0x15 : 0x12);
 	if (*(uint8_t *)(patchPoint + 1) == 0x85) { // jne
 		// replace entire call w/ nop
 		for (int i = 0; i < 6; i++) {
@@ -59,10 +104,59 @@ void PatchAPFS(KernelPatcher &patcher, size_t index, mach_vm_address_t address, 
 	MachInfo::setKernelWriting(false, KernelPatcher::kernelWriteLock);
 }
 
+mach_vm_address_t getKernelBase() {
+#ifdef LILU_COMPRESSION_SUPPORT
+	static constexpr const char *prelinkKernelPaths[7] {
+		// This is the usual kernel cache place, which often the best thing to use
+		"/System/Library/Caches/com.apple.kext.caches/Startup/kernelcache",
+		// Otherwise fallback to one of the prelinked kernels
+		// Since we always verify the LC_UUID value, trying the kernels could be done in any order.
+		"/System/Library/PrelinkedKernels/prelinkedkernel", // normal
+		"/macOS Install Data/Locked Files/Boot Files/prelinkedkernel", // 10.13 installer
+		"/com.apple.boot.R/prelinkedkernel", // 10.12+ fusion drive installer
+		"/com.apple.boot.S/System/Library/PrelinkedKernels/prelinkedkernel", // 10.11 fusion drive installer
+		"/com.apple.recovery.boot/prelinkedkernel", // recovery
+		"/kernelcache" // 10.7 installer
+	};
+#endif
+
+	static constexpr const char *kernelPaths[2] {
+		"/System/Library/Kernels/kernel",
+		"/mach_kernel"
+	};
+	static mach_vm_address_t kbase = 0;
+	if (!kbase) {
+		bool usePrelinkedCache = LILU_COMPRESSION_SUPPORT && WIOKit::usingPrelinkedCache();
+		
+		auto info = MachInfo::create(true, "kernel");
+		if (!info) {
+			return 0;
+		} else if ((info->init(usePrelinkedCache ? prelinkKernelPaths : kernelPaths, usePrelinkedCache ? arrsize(prelinkKernelPaths) : arrsize(kernelPaths), 0, false)) != KERN_SUCCESS) {
+			return 0;
+		}
+		
+		kbase = info->findKernelBase();
+		info->deinit();
+		MachInfo::deleter(info);
+	}
+	return kbase;
+}
+
 void PatchKernel(KernelPatcher &patcher) {
-	auto pivot_root = patcher.solveSymbol(0, "_pivot_root");
-	if (!KernelPatcher::findAndReplaceWithMask((void *) pivot_root, 32768, pivotRootOrig, sizeof(pivotRootOrig), pivotRootMask, sizeof(pivotRootMask), pivotRootReplace, sizeof(pivotRootReplace), pivotRootReplaceMask, sizeof(pivotRootReplaceMask))) {
-		panic("Failed to patch pivot_root!");
+	auto searchBase = patcher.solveSymbol(0, "_pivot_root");
+	size_t searchSize = 32768;
+	if (!searchBase) {
+		// our pattern is so unique that we can afford to do this
+		auto kInfo = (mach_header_64 *) getKernelBase();
+		auto kTextExec = get_segment(kInfo, "__TEXT_EXEC");
+		if (!kTextExec) panic("Failed to get kernel text segment!");
+		auto kText = get_section(kTextExec, "__text");
+		if (!kText) panic("Failed to get kernel text section!");
+		searchBase = (mach_vm_address_t) kInfo + kText->addr;
+		searchSize = kText->size;
+	}
+	if (!KernelPatcher::findAndReplaceWithMask((void *) searchBase, searchSize, pivotRootOrig, arrsize(pivotRootOrig), pivotRootMask, arrsize(pivotRootMask), pivotRootReplace, arrsize(pivotRootReplace), pivotRootReplaceMask, arrsize(pivotRootReplaceMask))) {
+		panic("Failed to patch RootVP!");
 	}
 }
 
